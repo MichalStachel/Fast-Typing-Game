3e6623d526eccb851a6a6ba6f43316370b82b6b5
unknown <michal.stachel@gmail.com>
2020-05-20 23:02:14 +0200

    finish logic,add some styles,remove routing

--- src/App.js
+++ src/App.js
@@ -1,32 +1,46 @@
 import React from "react";
-import { BrowserRouter as Router, Link } from "react-router-dom";
 import "./App.css";
-import Page from "./page.js";
 
 class App extends React.Component {
   state = {
     key: "",
     text: [],
+    textCopy: [],
+    writtenText: [],
     mistake: 0,
+    countDown: 60,
     userWordAmount: 10,
   };
 
-  componentDidMount() {
+  componentDidMount = () => {
     document.addEventListener("keydown", (e) => {
       this.setState({
         key: e.key,
       });
-
       if (this.state.text[0] === e.key) {
-        this.state.text.splice(0, 1);
-        return;
-      } else {
+        this.state.text.shift();
+      } else if (
+        this.state.text[0] !== e.key &&
+        this.state.text.length !== 0 &&
+        this.state.text.length !== this.state.textCopy.length
+      ) {
         this.setState({
-          mistake: this.state.mistake + 1,
+          mistake: ++this.state.mistake,
         });
       }
     });
-  }
+  };
+  componentDidUpdate = () => {
+    const { text, mistake, countDown } = this.state;
+    if (this.state.text.length === 1 && this.state.text[0] === this.state.key) {
+      this.state.writtenText = [];
+    }
+    if (countDown === 0) {
+      alert(`You lose :(
+        Letters remain: ${text.length}
+        Mistakes: ${mistake}`);
+    }
+  };
 
   addText = () => {
     fetch(
@@ -38,46 +52,125 @@ class App extends React.Component {
       .then((data) => {
         this.setState({
           text: data.text_out.toLowerCase().split("").slice(0, -5).slice(4),
+          textCopy: data.text_out.toLowerCase().split("").slice(0, -5).slice(4),
           mistake: 0,
         });
       });
+
+    this.setState({
+      flag: false,
+    });
   };
 
+  // Define amount of words you want to write
   userSettings = (e) => {
     this.setState({
       userWordAmount: e.target.value,
     });
   };
 
-  render() {
-    const { text, mistake } = this.state;
-    return (
-      <>
-        <Router basename={process.env.PUBLIC_URL}>
-          <div className="App">
-            {text}
-            <div>
-              <Link to={"/game"}>
-                {text.length === 0 ? (
-                  <button onClick={this.addText}>Start</button>
-                ) : null}
-              </Link>
-            </div>
-            {text.length === 0 ? (
-              <>
-                Words amount: <input type="text" onChange={this.userSettings} />
-              </>
-            ) : null}
-            {text.length !== 0 ? (
-              <p>
-                Mistake count: <h1>{mistake}</h1>
-              </p>
-            ) : null}
+  // Countdown timer
+  countDown = () => {
+    let countDown = 10;
+    const countInt = setInterval(() => {
+      countDown--;
+      this.setState({
+        countDown,
+      });
 
-            {<Page text={text} />}
+      if (countDown <= -1) {
+        clearInterval(countInt);
+        this.setState({
+          writtenText: [],
+          text: [],
+          textCopy: [],
+          userWordAmount: 10,
+          key: "",
+        });
+      }
+    }, 1000);
+  };
+
+  // Clear states
+  clear = () => {
+    if (this.state.text.length === 0) {
+      this.setState({
+        writtenText: [],
+        text: [],
+        textCopy: [],
+        userWordAmount: 10,
+        key: "",
+      });
+    }
+  };
+
+  // Add letter to writtenText Array
+  addLetter = () => {
+    let arr = [...this.state.writtenText];
+    const { text, writtenText, textCopy } = this.state;
+
+    if (text.length + 1 + writtenText.length === textCopy.length) {
+      arr.push(textCopy[writtenText.length]);
+
+      this.setState({
+        writtenText: arr,
+      });
+    }
+
+    console.log(text.length + 1, writtenText.length, textCopy.length);
+  };
+
+  render() {
+    const optionsArr = [20, 50, 100];
+    const { text, mistake, key, writtenText, countDown } = this.state;
+    return (
+      <div onChange={this.addLetter} className="center">
+        <div className="App">
+          <p className="textLeft"> {text}</p>
+          <p className="textLeft" id="writtenText">
+            {writtenText}
+          </p>
+          <div>
+            {text.length === 0 || (text.length === 1 && text[0] === key) ? (
+              <button onClick={this.addText}>Start</button>
+            ) : null}
           </div>
-        </Router>
-      </>
+          {text.length === 0 ? (
+            <ul>
+              {optionsArr.map((item) => (
+                <li>
+                  <button
+                    onClick={() => {
+                      this.setState({ userWordAmount: item });
+                    }}
+                  >
+                    {item}
+                  </button>
+                </li>
+              ))}
+            </ul>
+          ) : null}
+          {text.length !== 0 ? (
+            <p>
+              Mistakes: <span className="red">{mistake}</span>
+            </p>
+          ) : null}
+
+          {text.length !== 0 ? (
+            <p>
+              {countDown}
+              <input
+                type="text"
+                placeholder="Click HERE and start writing"
+                onClick={this.countDown}
+                onChange={this.clear}
+                id="myTextArea"
+                spellCheck="false"
+              />
+            </p>
+          ) : null}
+        </div>
+      </div>
     );
   }
 }
